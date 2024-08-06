b8bddf495b5fc86679c79b0eac13fe1443b975b9
02Ayushi <109408347+02Ayushi@users.noreply.github.com>
2024-08-06 21:04:33 +0800

    Add files via upload

---
+++ Personal Portfolio/main.js
@@ -0,0 +1,65 @@
+(function ($) {
+    "use strict"
+
+    //Navbar on Scrolling
+    $(window).scroll(function (){
+        if ($(this).scrollTop() > 200){
+            $('.navbar').fadeIn('slow').css('display','flex');
+        }
+        else{
+            $('.navbar').fadeOut('slow').css('display','none');
+        }
+    });
+
+    //Typed Initiate
+    if ($('.typed-text-output').length == 1){
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '), 
+            typeSpeed: 100,
+            backSpeed: 50,
+            smartBackspace: false,
+            loop: true
+        });
+
+    // Testimonial Carousel
+    $(document).ready(function(){
+        $(".testimonial-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1500,
+            dots: true,
+            loop: true,
+            items: 1
+        });
+    });
+
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    
+    $('#portfolio-filters li').on('click', function() {
+        $("#portfolio-filters li").removeClass('active');
+        $(this).addClass('active');
+    
+        portfolioIsotope.isotope({ filter: $(this).data('filter')});
+    });
+    
+
+    
+    //skill bar
+   
+   
+        $('.skill').waypoint(function() {
+            $('.progress .progress-bar').each(function() {
+                $(this).css("width", $(this).attr("aria-valuenow") + '%')
+            });
+        }, {offset: '80%'});
+        
+
+
+    }
+})(jQuery);
